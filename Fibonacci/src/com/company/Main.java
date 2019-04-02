aabe1df8822196e847eeaa49bdc9c76e98abe058
feni febriani <49176326+fenifebriani@users.noreply.github.com>
2019-04-02 10:01:19 +0700

    Update Main.java

--- Fibonacci/src/com/company/Main.java
+++ Fibonacci/src/com/company/Main.java
@@ -1,51 +1,19 @@
 package com.company;
 
 import java.util.Scanner;
-
 public class Main {
 
-    static int calculateSum(int n)
-    {
-        if (n <= 0)
-            return 0;
-
-        int fibo[]=new int[n+1];
-        fibo[0] = 0; fibo[1] = 1;
-
-        // Initialize result
-        int sum = fibo[0] + fibo[1];
-
-        // Add remaining terms
-        for (int i=2; i<=n; i++)
-        {
-            fibo[i] = fibo[i-1]+fibo[i-2];
-            sum += fibo[i];
-        }
-
-        return sum;
-    }
 
     public static void main(String[] args) {
-	// write your code here
+        new Main().calc(317811, 1);
+    }
 
-        Scanner ucok = new Scanner(System.in);
-        int i = 1, t1 = 0, t2 = 1;
-        System.out.println("Input Jumlah Angka Fibonacci: ");
-        int n = ucok.nextInt();
-        System.out.print("\nFirst " + n + " terms: \n");
-
-        while (i <= n)
-        {
-            System.out.print(t1 + " ");
-
-            int sum = t1 + t2;
-            t1 = t2;
-            t2 = sum;
-
-            i++;
+    public void calc(Integer start, Integer end) {
+        Integer f = (int) (start / ((1 + Math.sqrt(5)) / 2));
+        System.out.println(f);
+        if (f == end) {
+            return;
         }
-
-        System.out.println("\n\nSum of Fibonacci" +
-                " numbers is : "+ calculateSum(n));
+        calc(++f, end);
     }
 }
