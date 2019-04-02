d83e9181322ed63ae8b13ee4f179fd5b82bd28f5
Dwiki Lazzaro <47839921+Telyu@users.noreply.github.com>
2019-04-02 09:40:48 +0700

    Add files via upload

--- Fibonacci/src/com/company/Main.java
+++ Fibonacci/src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+
+    static int calculateSum(int n)
+    {
+        if (n <= 0)
+            return 0;
+
+        int fibo[]=new int[n+1];
+        fibo[0] = 0; fibo[1] = 1;
+
+        // Initialize result
+        int sum = fibo[0] + fibo[1];
+
+        // Add remaining terms
+        for (int i=2; i<=n; i++)
+        {
+            fibo[i] = fibo[i-1]+fibo[i-2];
+            sum += fibo[i];
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
 	// write your code here
 
@@ -22,5 +44,8 @@ public class Main {
 
             i++;
         }
+
+        System.out.println("\n\nSum of Fibonacci" +
+                " numbers is : "+ calculateSum(n));
     }
 }
