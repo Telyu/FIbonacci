aa15dc2e40ce4258d45930bc822df845ecc235df
Tomcatt1st <49178381+Tomcatt1st@users.noreply.github.com>
2019-04-02 10:20:27 +0700

    Update Main.java

--- Fibonacci/src/com/company/Main.java
+++ Fibonacci/src/com/company/Main.java
@@ -1,19 +1,81 @@
 package com.company;
 
 import java.util.Scanner;
+
 public class Main {
 
+    static int calculateSum(int n)
+    {
+        if (n <= 0)
+            return 0;
 
-    public static void main(String[] args) {
-        new Main().calc(317811, 1);
+        int fibo[]=new int[n+1];
+        fibo[0] = 0; fibo[1] = 1;
+
+        // Initialize result
+        int sum = fibo[0] + fibo[1];
+
+        // Add remaining terms
+        for (int i=2; i<=n; i++)
+        {
+            fibo[i] = fibo[i-1]+fibo[i-2];
+            sum += fibo[i];
+        }
+
+        return sum;
     }
 
-    public void calc(Integer start, Integer end) {
-        Integer f = (int) (start / ((1 + Math.sqrt(5)) / 2));
-        System.out.println(f);
-        if (f == end) {
-            return;
+    static void reverseFibonacci(int n)
+    {
+        int a[] = new int[n];
+
+        // assigning first and second elements
+        a[0] = 0;
+        a[1] = 1;
+
+        for (int i = 2; i < n; i++)
+        {
+
+            // storing sum in the
+            // preceding location
+            a[i] = a[i - 2] + a[i - 1];
         }
-        calc(++f, end);
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+
+            // printing array in
+            // reverse order
+            System.out.print(a[i] +" ");
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner ucok = new Scanner(System.in);
+        int i = 1, t1 = 0, t2 = 1;
+        System.out.println("Input Jumlah Angka Fibonacci: ");
+        int n = ucok.nextInt();
+        System.out.print("\nFirst " + n + " terms: \n");
+
+        while (i <= n)
+        {
+            System.out.print(t1 + " ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+
+            i++;
+        }
+
+        System.out.println("\n\nSum of Fibonacci" +
+                " numbers is : "+ calculateSum(n));
+
+
+        System.out.println("\nReverse of Fibonacci numbers is : ");
+        reverseFibonacci(n);
+
     }
 }
